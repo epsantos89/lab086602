442825ddc20b2f54a72eea00633b1fea14282b99
epsantos <elias.pereira.89@gmail.com>
2016-08-15 19:11:43 -0300

    #4

---
+++ src/main/java/br/univel/ejb/ProcessEntrega.java
@@ -0,0 +1,14 @@
+package br.univel.ejb;
+
+import javax.ejb.Stateless;
+
+import br.univel.classes.Entrega;
+
+@Stateless
+class ProcessEntrega {
+
+	public void processarEntrega (Entrega entrega) {
+
+    }
+
+}
---
+++ src/main/java/br/univel/ejb/ProcessVenda.java
@@ -0,0 +1,14 @@
+package br.univel.ejb;
+
+import javax.ejb.Stateless;
+
+import br.univel.classes.Venda;
+
+@Stateless
+class ProcessVenda {
+
+	public void processarVenda(Venda venda) {
+
+    }
+
+}
